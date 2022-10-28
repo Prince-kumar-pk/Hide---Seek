2968e59bd0ac33d8a52435b3f375607c3cc47594
Prince-kumar-pk <princekumar92ssm@gmail.com>
2022-10-28 18:33:32 +0530

    Game

--- hard.js
+++ hard.js
@@ -2,21 +2,22 @@ const cards = document.querySelectorAll(".memory-card");
 let life = document.getElementById("life");
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
-var miss_count = 0;
-var gift = 0;
-var lf = 4;
-
 let audioTurn = new Audio("music/ting.mp3")
 let gameover_audio = new Audio("music/gameover.mp3")
+var miss_count = 0;
+var gift = 0;
+var lf = 10;
 var sc ;
+
 function flipCard() {
  lf = lf - 1;
  life.innerHTML = lf;
  this.classList.add("flip");
-
-
-  // console.log(this.getAttribute("data-value"));
+ 
 audioTurn.play();
+  // console.log(this.getAttribute("data-value"));
+
+
 
   if (this.getAttribute("data-value") === "miss") {
     miss_count = miss_count + 1;
@@ -25,13 +26,15 @@ audioTurn.play();
     let x = document.getElementById("score");
     sc = gift*10;
     x.innerHTML = sc;
-    
+
+
   }else  {
     document.getElementById("text").innerHTML = "You click on Danger! Game Over";
     cards.forEach((card) => card.removeEventListener("click", flipCard));
     let btn = document.getElementById("replay");
     btn.style.display = "block";
     gameover_audio.play();
+    // console.log("its danger");
   }
 
 
@@ -45,23 +48,18 @@ check();
 
 function check(){
 
-  if (gift===3) {
-    console.log("checked");
+  if (gift===10) {
+    // console.log("checked");
     document.getElementById("text").innerHTML = "All Gifts Captured! You Win";
     cards.forEach((card) => card.removeEventListener("click", flipCard));
-    let btn = document.getElementById("replay");
-    btn.style.display = "block";
     gameover_audio.play();
-
   }
   if(lf === 0){
     document.getElementById("text").innerHTML = "You life finished! Game Over";
     cards.forEach((card) => card.removeEventListener("click", flipCard));
-    //alert("Your Game is over now !! if you want to play again press replay button");
     let btn = document.getElementById("replay");
     btn.style.display = "block";
     gameover_audio.play();
-
   }
 }
 
@@ -77,8 +75,5 @@ function check(){
 
 function reload()
 {
-  window.location.href = "./easy.html";
+  window.location.href = "./intermediate.html";
 }
-
-
-
